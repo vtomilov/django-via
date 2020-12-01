98601fd31af2d6e25ca6c0383b5c9ea6923aa7a9
Виталий Томилов <tomilov.va93@gmail.com>
2020-12-01 16:42:27 +0300

    Regions by user

--- via_django/via/admin.py
+++ via_django/via/admin.py
@@ -4,4 +4,26 @@ from django.contrib import admin
 from .models import Project, Image
 
 admin.site.register(Project)
-admin.site.register(Image)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    search_fields = ['project', 'edited_by']
+    list_display = ('filename', 'project', 'edited_by', 'region_count', 'regions_by_user', 'edit_date')
+    list_filter = ('edited_by', 'project')
+
+    def region_count(self, instance):
+        return len(instance.regions)
+
+    def regions_by_user(self, instance):
+        stats = {
+            "unknown": 0
+        }
+        for r in instance.regions:
+            if "user_edited" in r:
+                if r["user_edited"] not in stats:
+                    stats[r["user_edited"]] = 0
+                stats[r["user_edited"]] += 1
+            else:
+                stats["unknown"] += 1
+        return stats
